d881860219ba7667a08177869762ba1c55a8d74c
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-10-29 20:23:38 +0330

    add location list component

---
+++ hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export default function useFetch(url, query = "") {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${url}?${query}`);
+        setData(data);
+      } catch (error) {
+        toast.error(error?.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url, query]);
+
+  return { isLoading, data };
+}
