fe5080149abab1acf176cf61660dfedc5e59f789
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-10-30 12:46:26 +0330

    Fix type and add loader to location list

--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -1,9 +1,11 @@
 import useFetch from "../../../hooks/useFetch";
 
+import Loader from "../Loader/Loader";
+
 const LocationList = () => {
   const { isLoading, data } = useFetch("http://localhost:5000/hotels", "");
 
-  if (isLoading) return <p>loading...</p>;
+  if (isLoading) return <Loader />;
 
   return (
     <div className="nearbyLocation">
@@ -16,7 +18,7 @@ const LocationList = () => {
               <div className="locationItemDesc">
                 <p className="location">{item.smart_location}</p>
                 <p className="name">{item.name}</p>
-                <p className="name">
+                <p className="price">
                   €&nbsp;{item.price}&nbsp;
                   <span>night</span>
                 </p>
